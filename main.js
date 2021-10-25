7538009c12616b67ae056335c844c37a41fe0656
hankwu99 <91450049+hankwu99@users.noreply.github.com>
2021-10-25 15:57:40 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function newCourse()
     let classLocation = document.getElementById("input2").value;
     let classType = document.getElementById("type").value;
     alert(`${className} ${classLocation} ${classType}`);
+    var el = document.querySelector('.list');
+    el.innerHTML = `<li style = "color: rgb(164, 255, 192);">課程名稱: ${className} --- 教室位置: ${classLocation}</li>`;
 }
 var submit = document.getElementById("button");
 submit.onclick = newCourse;
